b58612380d7822db815097f5efc865498b92f80e
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-12-26 15:20:16 -0300

    :rocket: Configura rotas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import Teste from '~/teste';
+import Routes from './routes';
 
 // import { Container } from './styles';
 
 export default function App() {
-  return <Teste />;
+  return <Routes />;
 }
