bcd940961b8a4bf6615093506813bc79093a59e7
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-12-26 14:36:33 -0300

    :rocket: Configura Root Import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Teste from '~/teste';
+
 // import { Container } from './styles';
 
 export default function App() {
-  return <View />;
+  return <Teste />;
 }
